e2854c3f59f7035ef12cd1f8116364aca2b928d0
Maciej Wójs <misiek111403@gmail.com>
2024-12-10 13:16:11 +0100

    Workflow test commit

---
+++ Matrix.hpp
@@ -0,0 +1,12 @@
+#ifndef A6AD9251_8722_4046_9226_D571B42EF548
+#define A6AD9251_8722_4046_9226_D571B42EF548
+
+class Matrix {
+    public:
+    Matrix();
+    ~Matrix();
+};
+
+
+
+#endif /* A6AD9251_8722_4046_9226_D571B42EF548 */
