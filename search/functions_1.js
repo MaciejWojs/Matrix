426b9f0d1a0848d97b8668391a9e2fe0a7ece1a6
MaciejWojs <MaciejWojs@users.noreply.github.com>
2024-12-19 19:50:00 +0000

    deploy: 98da6c25af4bfa2ec550f083773fc89a398a3772

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['check_5fshape_0',['check_shape',['../classMatrix.html#a3edf27d6ae6def59c1beb8217e983c02',1,'Matrix']]],
-  ['column_1',['column',['../classMatrix.html#a86940a4cff1fb746d0a330ff9d40a5c5',1,'Matrix']]]
+  ['clear_1',['clear',['../classMatrix.html#a81253411857dda69694b32e4fbf5d0b4',1,'Matrix']]],
+  ['column_2',['column',['../classMatrix.html#a86940a4cff1fb746d0a330ff9d40a5c5',1,'Matrix']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['pick_5frandom_5fcolumn_0',['pick_Random_Column',['../classMatrix.html#af0f95956b71e1b46809764559bb2cb04',1,'Matrix']]],
   ['pick_5frandom_5frow_1',['pick_Random_Row',['../classMatrix.html#a79bdae65ce7ee933532d79894f900351',1,'Matrix']]],
-  ['print_2',['print',['../classMatrix.html#a99ba97122b8fdd54e95290caf80fc8e2',1,'Matrix']]]
+  ['print_2',['print',['../classMatrix.html#a26b8608bcfc163c7075282d87e86744a',1,'Matrix']]]
 ];
