459c9666520d62550bc9ad80ff1786f424d2052a
MaciejWojs <MaciejWojs@users.noreply.github.com>
2024-12-19 22:49:40 +0000

    deploy: bb9727230a30e27f4dd09a3a8267348188b9ff95

--- Matrix-operators_8cpp.js
+++ Matrix-operators_8cpp.js
@@ -3,6 +3,7 @@ var Matrix_operators_8cpp =
     [ "operator*", "Matrix-operators_8cpp.html#ac4b9085349415e5f97dd35927843ed51", null ],
     [ "operator+", "Matrix-operators_8cpp.html#a6d6c2912d7d078cdaacd10f4eab3cb1d", null ],
     [ "operator-", "Matrix-operators_8cpp.html#a8f20cfac4af1a1b21a75d22d3a9014ea", null ],
+    [ "operator/", "Matrix-operators_8cpp.html#a87825d2733a4df58538e97d3ea6ed816", null ],
     [ "operator<<", "Matrix-operators_8cpp.html#a5bb1cbd42cb90fc74ede8d1f6f253ca5", null ],
     [ "operator>>", "Matrix-operators_8cpp.html#a11ec4d4662441284891dd06da0536a94", null ]
 ];
--- search/related_0.js
+++ search/related_0.js
@@ -3,6 +3,7 @@ var searchData=
   ['operator_2a_0',['operator*',['../classMatrix.html#ac4b9085349415e5f97dd35927843ed51',1,'Matrix']]],
   ['operator_2b_1',['operator+',['../classMatrix.html#a6d6c2912d7d078cdaacd10f4eab3cb1d',1,'Matrix']]],
   ['operator_2d_2',['operator-',['../classMatrix.html#a8f20cfac4af1a1b21a75d22d3a9014ea',1,'Matrix']]],
-  ['operator_3c_3c_3',['operator&lt;&lt;',['../classMatrix.html#a6148b439ea866b655012a16d03fefb07',1,'Matrix']]],
-  ['operator_3e_3e_4',['operator&gt;&gt;',['../classMatrix.html#a11ec4d4662441284891dd06da0536a94',1,'Matrix']]]
+  ['operator_2f_3',['operator/',['../classMatrix.html#a87825d2733a4df58538e97d3ea6ed816',1,'Matrix']]],
+  ['operator_3c_3c_4',['operator&lt;&lt;',['../classMatrix.html#a6148b439ea866b655012a16d03fefb07',1,'Matrix']]],
+  ['operator_3e_3e_5',['operator&gt;&gt;',['../classMatrix.html#a11ec4d4662441284891dd06da0536a94',1,'Matrix']]]
 ];
