0b2359c1b52e1d39edcd592a82884e7436561855
ar00k <arkadiusz.ryczek@onet.pl>
2024-12-20 12:20:10 +0100

    method call

--- main.cpp
+++ main.cpp
@@ -9,54 +9,59 @@ int main() {
     const int size = 2;
     int array[size * size] = { 1, 2, 3, 4 };
     Matrix matrix(size, array);
-    // m.random();
 
     Matrix m2(matrix);
 
     std::cout << "Original Matrix 1:\n";
     matrix.print();
+
     std::cout << "Matrix 1 after adding 1 to all elements:\n";
     matrix += 1;
     matrix.print();
+
     std::cout << "Matrix 1 after subtracting 2 from all elements:\n";
     matrix -= 2;
     matrix.print();
+
     std::cout << "Matrix 1 after subtracting -20 from all elements:\n";
     matrix -= -20;
     matrix.print();
+
     std::cout << "Matrix 1 after multiplying by 2:\n";
     matrix *= 2;
     matrix.print();
+
     std::cout << "Matrix 1 transposed:\n";
     matrix.transpose();
     matrix.print();
 
-    std::cout << "Matrix 2:\n";
+    std::cout << "Matrix 2 (copy of Matrix 1):\n";
     m2.print();
-
 
     std::cout << "\nMatrix 1 after randomizing with attribute 5:\n";
     matrix.random(5);
+    matrix.print();
 
     const int s_size = 5;
     int s_array[s_size * s_size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     int smaller_array[s_size] = { 1 };
     Matrix s(s_size, s_array);
-    std::cout << "\nNew Matrix\n";
+
+    std::cout << "\nNew Matrix:\n";
     s.print();
+
     std::cout << "\nMatrix clearing:\n";
     s.clear();
     s.print();
+
     std::cout << "\nMatrix diagonal:\n";
     s.diagonal(smaller_array);
     s.print();
 
     std::cout << "\nMatrix after filling diagonal with array:\n";
-
     s.print();
 
     s.diagonal_k(array, 1);
-
     std::cout << "\nMatrix after filling diagonal with array and k = 1:\n";
     s.print();
 
@@ -79,6 +84,18 @@ int main() {
     s.clear();
     s.print();
 
-    s.fill_above_diagonal().print().clear().print().fill_under_diagonal().print();
+    std::cout << "\nMatrix after filling above diagonal:\n";
+    s.fill_above_diagonal();
+    s.print();
+
+    std::cout << "\nMatrix after filling under diagonal:\n";
+    s.clear();
+    s.fill_under_diagonal();
+    s.print();
+
+    std::cout << "\nMatrix after setting checker shape:\n";
+    s.clear();
+    s.check_shape();
+    s.print();
     return 0;
 }
